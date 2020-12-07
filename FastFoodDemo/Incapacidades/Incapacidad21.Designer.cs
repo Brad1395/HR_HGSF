f5e215d3b38cdfc7e43b96a5b42876604142c543
Bradly <bradly.aguilera@gmail.com>
2020-12-07 09:09:03 -0600

    Carpeta-Incapacides

--- FastFoodDemo/Incapacidades/Incapacidad21.Designer.cs
+++ FastFoodDemo/Incapacidades/Incapacidad21.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RecursoHumano {
+namespace RecursoHumano.Incapacidades {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RecursoHumano {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RecursoHumano.Incapacidad2", typeof(Incapacidad2).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RecursoHumano.Incapacidades.Incapacidad2", typeof(Incapacidad2).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- FastFoodDemo/Incapacidades/Incapacidad31.Designer.cs
+++ FastFoodDemo/Incapacidades/Incapacidad31.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RecursoHumano {
+namespace RecursoHumano.Incapacidades {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace RecursoHumano {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Incapacidad {
+    internal class Incapacidad3 {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Incapacidad() {
+        internal Incapacidad3() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace RecursoHumano {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RecursoHumano.Incapacidad", typeof(Incapacidad).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RecursoHumano.Incapacidades.Incapacidad3", typeof(Incapacidad3).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
